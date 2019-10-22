f74b8ce780048a80cc50fd106af91fb3dc3ba534
Student <example@example.com>
2019-10-22 10:16:07 -0700

    add dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     $("#walrus-showing").slideToggle();
     $("#walrus-hidden").slideToggle();
   });
+  $("#darkmode").click(function() {
+    $("body").toggleClass("background-color-regular");
+    $("body").toggleClass("background-color-dark");
+  });
 });
